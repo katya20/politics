df51c89cf772c85d8ebea2bad3131ef74c142d66
katya20 <kpanchenko21@gmail.com>
2016-12-06 08:50:25 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ $.ajax({
   method: "GET",
   url: "https://www.govtrack.us/api/v2/person",
   success: function(data) {    
+
     data.objects.forEach(function(object) {
       console.log(object)
     })
